ecf2dfe397399e80059b9bedf9508289de11a821
michalbaldyga <michalbaldyga01@gmail.com>
2023-02-14 22:12:11 +0100

    Main extension

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
+import pandas as pd
+from data_processing import parse_data
+
+
 if __name__ == '__main__':
-    print()
+    raw_data = pd.read_csv('iris.data', header=None)
+    flowers = parse_data(raw_data)
