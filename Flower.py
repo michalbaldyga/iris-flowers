4606bcb8f2c7ac7060ad04c3c483dc18e65c5158
michalbaldyga <michal.baldyga01@gmail.com>
2023-02-15 13:50:40 +0100

    Add Flower class

---
+++ Flower.py
@@ -0,0 +1,5 @@
+class Flower:
+    """Exemplary flower object: ([5.1, 3.5, 1.4, 0.2], 'Iris-setosa')"""
+    def __init__(self, point, label):
+        self.point = point
+        self.label = label
